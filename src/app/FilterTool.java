43e9d3a3adac4f8d1ff966be96ace089b57015f6
maclean <gilleain.torrance@gmail.com>
2016-03-02 13:27:19 +0000

    Improve tooling

--- src/app/FilterTool.java
+++ src/app/FilterTool.java
@@ -8,6 +8,7 @@ import org.openscience.cdk.DefaultChemObjectBuilder;
 import org.openscience.cdk.exception.CDKException;
 import org.openscience.cdk.interfaces.IAtomContainer;
 import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.signature.MoleculeSignature;
 import org.openscience.cdk.smsd.Isomorphism;
 import org.openscience.cdk.smsd.interfaces.Algorithm;
 
@@ -16,16 +17,68 @@ import io.IteratingACPReader;
 
 public class FilterTool {
     
-    public static void look(String toLookFor, File toLookIn) throws IOException, CDKException {
+    private interface Matcher {
+        public boolean matches(IAtomContainer target);
+    }
+    
+    private class SMSDMatcher implements Matcher {
+        
+        Isomorphism isomorphism;
+        
+        IAtomContainer query;
+        
+        public SMSDMatcher(IAtomContainer query) {
+            this.query = query;
+        }
+
+        @Override
+        public boolean matches(IAtomContainer target) {
+            isomorphism = new Isomorphism(Algorithm.MCSPlus, true);
+            try {
+                isomorphism.init(query, target, true, true);
+            } catch (CDKException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+            return isomorphism.isSubgraph();
+        }
+    }
+    
+    private class SignatureMatcher implements Matcher {
+        String queryString;
+        
+        public SignatureMatcher(IAtomContainer query) {
+            this.queryString = new MoleculeSignature(query).toCanonicalString();
+        }
+
+        @Override
+        public boolean matches(IAtomContainer target) {
+            MoleculeSignature targetSig = new MoleculeSignature(target);
+            return queryString.equals(targetSig.toCanonicalString());
+        }
+        
+        
+    }
+    
+    public void look(String flag, String toLookFor, File toLookIn) throws IOException {
         System.out.println("looking for " + toLookFor + " in " + toLookIn);
         IChemObjectBuilder builder = DefaultChemObjectBuilder.getInstance(); 
         IAtomContainer query = AtomContainerPrinter.fromString(toLookFor, builder);
+        
+        Matcher matcher;
+        if (flag.equals("-i")) {
+            matcher = new SignatureMatcher(query);
+        } else if (flag.equals("-s")) {
+            matcher = new SMSDMatcher(query);
+        } else {
+            System.err.println("Flag must be -i (isomorphism) or -s (subgraph)");
+            return;
+        }
+        
         IteratingACPReader reader = new IteratingACPReader(new FileInputStream(toLookIn), builder);
-        Isomorphism isomorphism = new Isomorphism(Algorithm.TurboSubStructure, true);
         while (reader.hasNext()) {
             IAtomContainer target = reader.next();
-            isomorphism.init(query, target, true, true);
-            if (isomorphism.isSubgraph()) {
+            if (matcher.matches(target)) {
                 System.out.println(AtomContainerPrinter.toString(target));
             }
         }
@@ -33,8 +86,9 @@ public class FilterTool {
     }
 
     public static void main(String[] args) throws IOException, CDKException {
-        String toFind = args[0];
-        String toLook = args[1];
-        look(toFind, new File(toLook));
+        String flag = args[0];
+        String toFind = args[1];
+        String toLook = args[2];
+        new FilterTool().look(flag, toFind, new File(toLook));
     }
 }
--- src/app/ViewTool.java
+++ src/app/ViewTool.java
@@ -7,9 +7,18 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.openscience.cdk.DefaultChemObjectBuilder;
+import org.openscience.cdk.atomtype.CDKAtomTypeMatcher;
+import org.openscience.cdk.atomtype.IAtomTypeMatcher;
 import org.openscience.cdk.depict.DepictionGenerator;
 import org.openscience.cdk.exception.CDKException;
+import org.openscience.cdk.interfaces.IAtom;
 import org.openscience.cdk.interfaces.IAtomContainer;
+import org.openscience.cdk.interfaces.IAtomType;
+import org.openscience.cdk.interfaces.IChemObjectBuilder;
+import org.openscience.cdk.silent.SilentChemObjectBuilder;
+import org.openscience.cdk.smiles.SmilesGenerator;
+import org.openscience.cdk.tools.CDKHydrogenAdder;
+import org.openscience.cdk.tools.manipulator.AtomTypeManipulator;
 
 import io.IteratingACPReader;
 
@@ -20,12 +29,31 @@ public class ViewTool {
         IteratingACPReader reader = new IteratingACPReader(
                 new FileInputStream(inputFilename), DefaultChemObjectBuilder.getInstance());
         
+        
         while (reader.hasNext()) {
             IAtomContainer container = reader.next();
+            try {
+                IAtomTypeMatcher matcher = getMatcher();
+                for (IAtom atom : container.atoms()) {
+                    IAtomType type = matcher.findMatchingAtomType(container, atom);
+                    AtomTypeManipulator.configure(atom, type);
+                }
+                CDKHydrogenAdder.getInstance(
+                        SilentChemObjectBuilder.getInstance()).addImplicitHydrogens(container);
+                System.out.println(SmilesGenerator.unique().create(container));
+            } catch (CDKException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
             containers.add(container);
         }
         reader.close();
         drawOut(containers, new File(outputFilename));
+    }
+    
+    private static IAtomTypeMatcher getMatcher() {
+        IChemObjectBuilder builder = SilentChemObjectBuilder.getInstance();
+        return CDKAtomTypeMatcher.getInstance(builder);
     }
     
     private static void drawOut(List<IAtomContainer> containers, File outputFile) {
